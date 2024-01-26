b156c24e1acead33c957a3a8173b4916a8845865
dohokwak <kwakdoho@gmail.com>
2024-01-27 04:09:02 +0900

    pharmacy-10

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(port, function(){
     console.log("HTML 서버 시작됨");
 });
 
-// http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=IZZGckj3W3TCPtsbRQQSADS7mFZASo0uNz58OHEtLXL4mDv3hSeSlATBPlNvhIPLhJ8X9W0cIBH%2BV1GQBgIhDA%3D%3D&Q0=서울특별시&Q1=강남구&QT=1&QN=삼성약국&ORD=NAME&pageNo=1&numOfRows=10
+// http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=IZZGckj3W3TCPtsbRQQSADS7mFZASo0uNz58OHEtLXL4mDv3hSeSlATBPlNvhIPLhJ8X9W0cIBH%2BV1GQBgIhDA%3D%3D&Q0=?��?��?��별시&Q1=강남�?&QT=1&QN=?��?��?���?&ORD=NAME&pageNo=1&numOfRows=10
 
 app.get("/pharmach_list", (req, res) => {
         let api = async() => {
@@ -26,7 +26,7 @@ app.get("/pharmach_list", (req, res) => {
                         "pageNo": req.query.pageNo,
                         "numOfRows": req.query.numOfRows
                     }
-                    headers: {"Accept-Encoding": "application/json"}
+                    
                 })
             }
             catch(e) {
@@ -36,6 +36,6 @@ app.get("/pharmach_list", (req, res) => {
         }
         api().then((response) => {
             res.setHeader("Access-Control-Allow-Origin", "*");
-            res.json(response?.data.response?.body);
+            res.json(response.data.response.body);
         });
 });
